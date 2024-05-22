79156799ecbb599001e9b0362123cead968e2714
Jasczlv <ianis.cozlov@gmail.com>
2024-05-22 16:27:15 +0200

    ADD vite & img

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+import "./bootstrap";
+
+//importo scss usando l'alias che ho create in precedenza in vite.config.js
+import "~resources/scss/app.scss";
+
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
+//importo la tutti i contenuti della cartella img
+import.meta.glob(["../img/**"]);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 @section('content')
 <div>
+    @vite('resources/js/app.js')
     <h1>
         Home
     </h1>
+    <img class="gato" src="{{Vite::asset('resources/img/gato.gif') }}" alt="">
 </div>
 @endsection
